0dc2d852e170e9ba3589cdfc6aeff422d0c2e41c
HarveyPy-1 <126327787+HarveyPy-1@users.noreply.github.com>
2023-08-25 18:48:41 +0100

    Build responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,29 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 function App() {
-  return <Button colorScheme="blue">Button</Button>
+	return (
+		// Check out HTML Grid
+		<Grid
+			// This is just like mediaquery. Check out chakra's documentation to understand more
+			templateAreas={{
+				base: `"nav" "main"`,
+				lg: `"nav nav" "aside main"`, // Larger than 1024px
+			}}>
+			<GridItem area="nav" bg="coral">
+				Nav
+			</GridItem>
+      {
+        // Show helps to determine which component shows at which condition (Documentation)
+      }
+			<Show above="lg">
+				<GridItem area="aside" bg="gold">
+					Aside
+				</GridItem>
+			</Show>
+			<GridItem area="main" bg="dodgerblue">
+				Main
+			</GridItem>
+		</Grid>
+	);
 }
 
 export default App;
