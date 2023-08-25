b4c96be2739b6cea4d7a2334af3a0c38442918fb
HarveyPy-1 <126327787+HarveyPy-1@users.noreply.github.com>
2023-08-25 19:54:47 +0100

    Build dark mode selector

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,15 @@
+import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+import { BsFillMoonFill } from 'react-icons/bs'
+import { PiSunLight } from 'react-icons/pi'
+
+const ColorModeSwitch = () => {
+	const { toggleColorMode, colorMode} = useColorMode();
+	return (
+		<HStack>
+			<Switch colorScheme="green" isChecked={colorMode === 'dark'} onChange={toggleColorMode}/>
+			<Text>{ (colorMode === 'dark') ? <BsFillMoonFill size="18"/> : <PiSunLight size="25" color="green"/>}</Text>
+		</HStack>
+	);
+};
+
+export default ColorModeSwitch;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,13 @@
-import { HStack, Image, Text } from "@chakra-ui/react"
+import { HStack, Image } from "@chakra-ui/react"
 import logo from '../assets/logo.webp' //You have to immport the logo image
+import ColorModeSwitch from "./ColorModeSwitch"
 
 const NavBar = () => {
   return (
     // Makes things horizontally stacked
-    <HStack>
+    <HStack justifyContent='space-between' padding='10px'>
         <Image src={logo} boxSize={"60px"}/>
-        <Text>Nav Bar</Text>
+        <ColorModeSwitch />
     </HStack>
   )
 }
