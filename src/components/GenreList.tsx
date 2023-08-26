a37d1c3104ce1456ef0c0e24148d69f85b0f3c28
HarveyPy-1 <126327787+HarveyPy-1@users.noreply.github.com>
2023-08-26 19:28:41 +0100

    Highlight selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,28 +1,39 @@
-import { Button, HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
+import {
+	Button,
+	HStack,
+	Image,
+	List,
+	ListItem,
+	Spinner,
+  textDecoration,
+} from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenre";
 import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
-  onselectGenre: (genre: Genre) => void
+	onselectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null
 }
 
-const GenreList = ({ onselectGenre}: Props) => {
+const GenreList = ({ selectedGenre, onselectGenre }: Props) => {
 	const { data, isLoading, error } = useGenres();
 
-  if (isLoading) return <Spinner />
+	if (isLoading) return <Spinner />;
 
-  if (error) return null;
+	if (error) return null;
 	return (
 		<List>
 			{data.map((genre) => (
-				<ListItem key={genre.id} paddingY='5px'>
+				<ListItem key={genre.id} paddingY="5px">
 					<HStack>
 						<Image
 							boxSize="32px"
 							borderRadius={8}
 							src={getCroppedImageUrl(genre.image_background)}
 						/>
-            <Button onClick={() => onselectGenre(genre)} variant='link'>{genre.name}</Button>
+						<Button fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} textDecoration={genre.id === selectedGenre?.id ? 'underline' : 'none'} onClick={() => onselectGenre(genre)} variant="link">
+							{genre.name}
+						</Button>
 					</HStack>
 				</ListItem>
 			))}
