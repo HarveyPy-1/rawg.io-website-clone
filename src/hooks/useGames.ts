c3ce78145acb5967e0a7ed9220b2926a44a8b9ed
HarveyPy-1 <126327787+HarveyPy-1@users.noreply.github.com>
2023-08-26 16:00:31 +0100

    Show loading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -5,18 +5,18 @@ import { CanceledError } from "axios";
 // We are creating this custom hook to get separation of concerns. GameGrid should only focus on returning markups and handling user interactions. All the other logic for retrieving the games from the url, and other http requests can be separated here.
 
 export interface Platform {
-    id: number
-    name: string;
-    slug: string;
+	id: number;
+	name: string;
+	slug: string;
 }
 
 // We had to create this game interface to tell 'results' below, the type of data it should expect.
 export interface Game {
 	id: number;
 	name: string;
-    background_image: string;
-    parent_platforms: { platform: Platform}[] //This is just how data from the API is structured. The defined platform interface is inside another array called platform
-    metacritic: number;
+	background_image: string;
+	parent_platforms: { platform: Platform }[]; //This is just how data from the API is structured. The defined platform interface is inside another array called platform
+	metacritic: number;
 }
 
 // The interface is based on what the api doc says the response will look like
@@ -28,25 +28,31 @@ interface FetchGamesResponse {
 const useGames = () => {
 	const [games, setGames] = useState<Game[]>([]);
 	const [error, setError] = useState("");
+	const [isLoading, setLoading] = useState(false);
 
 	useEffect(() => {
 		const controller = new AbortController();
 
+		setLoading(true);
 		// <FetchGamesResponse> remember, is necessary for autocomplete purposes.
 		apiClients
 			.get<FetchGamesResponse>("/games", { signal: controller.signal })
-			.then((res) => setGames(res.data.results))
-            // It shows 'canceled' unless you add the 'if' statement
+			.then((res) => {
+				setGames(res.data.results);
+				setLoading(false);
+			})
+			// It shows 'canceled' unless you add the 'if' statement
 			.catch((err) => {
 				if (err instanceof CanceledError) return;
 				setError(err.message);
+                setLoading(false)
 			});
 
 		// Clean up code
 		return () => controller.abort();
 	}, []);
 
-	return { games, error };
+	return { games, error, isLoading };
 };
 
 export default useGames;
