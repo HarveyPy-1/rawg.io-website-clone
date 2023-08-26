574a7b909a4610cdaf875b6abb5548d032d973f5
HarveyPy-1 <126327787+HarveyPy-1@users.noreply.github.com>
2023-08-26 18:41:31 +0100

    Show spinner while fetching genres

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,7 +3,6 @@ import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
-import { useEffect, useState } from "react";
 
 interface Props {
 	game: Game;
