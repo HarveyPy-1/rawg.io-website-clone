707b716aecb11df116d1ddbdfd8135885b5d05de
HarveyPy-1 <126327787+HarveyPy-1@users.noreply.github.com>
2023-08-26 14:46:02 +0100

    Display games in grid

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -4,11 +4,18 @@ import { CanceledError } from "axios";
 
 // We are creating this custom hook to get separation of concerns. GameGrid should only focus on returning markups and handling user interactions. All the other logic for retrieving the games from the url, and other http requests can be separated here.
 
+export interface Platform {
+    id: number
+    name: string;
+    slug: string;
+}
+
 // We had to create this game interface to tell 'results' below, the type of data it should expect.
 export interface Game {
 	id: number;
 	name: string;
     background_image: string;
+    parent_platforms: { platform: Platform}[] //This is just how data from the API is structured. The defined platform interface is inside another array called platform
 }
 
 // The interface is based on what the api doc says the response will look like
