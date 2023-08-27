0a6a4272e455b1e9de0b00e7f476a56bc87228dd
HarveyPy-1 <126327787+HarveyPy-1@users.noreply.github.com>
2023-08-27 19:58:49 +0100

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Flex, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
+import { Flex, Grid, GridItem, Show, Text } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
@@ -20,61 +20,65 @@ export interface GameQuery {
 function App() {
 	const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
 	return (
-		<Grid
-			templateAreas={{
-				base: `"nav" "main"`,
-				lg: `"nav nav" "aside main"`, // Larger than 1024px
-			}}
-			templateColumns={{
-				base: "1fr",
-				lg: "200px 1fr",
-			}}>
-			<GridItem area="nav">
-				<NavBar
-					onSearch={(searchText) =>
-						setGameQuery({ ...gameQuery, searchText })
-					}></NavBar>
-			</GridItem>
-			<Show above="lg">
-				<GridItem area="aside" paddingX={5}>
-					<GenreList
-						selectedGenre={gameQuery.genre}
-						onselectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
-					/>
+		<>
+			<Grid
+				templateAreas={{
+					base: `"nav" "main"`,
+					lg: `"nav nav" "aside main"`, // Larger than 1024px
+				}}
+				templateColumns={{
+					base: "1fr",
+					lg: "200px 1fr",
+				}}>
+				<GridItem area="nav">
+					<NavBar
+						onSearch={(searchText) =>
+							setGameQuery({ ...gameQuery, searchText })
+						}></NavBar>
 				</GridItem>
-			</Show>
-			<GridItem area="main">
-				<GameHeading gameQuery={gameQuery} />
-
-				<Flex
-					justifyItems={"left"}
-					paddingX="20px"
-					wrap={"wrap"}
-					marginBottom={2}>
-					<PlatformSelector
-						selectedPlatform={gameQuery.platform}
-						onSelectPlatform={(platform) =>
-							setGameQuery({ ...gameQuery, platform })
-						}
-					/>
-					<SortSelector
-						sortOrder={gameQuery.sortOrder}
-						onSelectSortOrder={(sortOrder) =>
-							setGameQuery({ ...gameQuery, sortOrder })
-						}
-					/>
-					<Show below="lg">
-						<GenreDropDown
+				<Show above="lg">
+					<GridItem area="aside" paddingX={5}>
+						<GenreList
 							selectedGenre={gameQuery.genre}
-							onselectGenre={(genre) =>
-								setGameQuery({ ...gameQuery, genre })
-							}></GenreDropDown>
-					</Show>
-				</Flex>
-				{/* <HStack marginX={5}></HStack> */}
-				<GameGrid gameQuery={gameQuery} />
-			</GridItem>
-		</Grid>
+							onselectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
+						/>
+					</GridItem>
+				</Show>
+				<GridItem area="main">
+					<GameHeading gameQuery={gameQuery} />
+
+					<Flex
+						justifyItems={"left"}
+						paddingX="20px"
+						wrap={"wrap"}
+						marginBottom={2}>
+						<PlatformSelector
+							selectedPlatform={gameQuery.platform}
+							onSelectPlatform={(platform) =>
+								setGameQuery({ ...gameQuery, platform })
+							}
+						/>
+						<SortSelector
+							sortOrder={gameQuery.sortOrder}
+							onSelectSortOrder={(sortOrder) =>
+								setGameQuery({ ...gameQuery, sortOrder })
+							}
+						/>
+						<Show below="lg">
+							<GenreDropDown
+								selectedGenre={gameQuery.genre}
+								onselectGenre={(genre) =>
+									setGameQuery({ ...gameQuery, genre })
+								}></GenreDropDown>
+						</Show>
+					</Flex>
+					<GameGrid gameQuery={gameQuery} />
+				</GridItem>
+			</Grid>
+			<Flex justifyContent="center" marginBottom={3}>
+				<Text as="h2">Harvey Productions ©️2023. </Text>
+			</Flex>
+		</>
 	);
 }
 
