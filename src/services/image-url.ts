c9721eeb46bf9ad4102b07b8ee99a8b0a95f399b
HarveyPy-1 <126327787+HarveyPy-1@users.noreply.github.com>
2023-08-27 15:09:06 +0100

    Add sorting games logic

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,14 +1,16 @@
 // This service will help crop the images to smaller sizes to make the page load faster and save data. Luckily the api itself supports cropping images.
 
 const getCroppedImageUrl = (url: string) => {
-    //The targeted word where we want to modify the url
-    const target = 'media/'
+	if (!url) return "";
 
-    // The position of the target in the url + the number of the letters. Basically, we'll end up behind media/
-    const index = url.indexOf(target) + target.length
+	//The targeted word where we want to modify the url
+	const target = "media/";
 
-    // Modify the url after getting the right position (the modifier crops the image. It comes with the api)
-    return url.slice(0, index) + 'crop/600/400/' + url.slice(index)
-}
+	// The position of the target in the url + the number of the letters. Basically, we'll end up behind media/
+	const index = url.indexOf(target) + target.length;
+
+	// Modify the url after getting the right position (the modifier crops the image. It comes with the api)
+	return url.slice(0, index) + "crop/600/400/" + url.slice(index);
+};
 
 export default getCroppedImageUrl;
